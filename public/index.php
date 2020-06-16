e87155f93eed4c27308abf338c17a883834a74b1
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-16 21:42:46 +0300

    added config, header, footer, style, javascript

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello June 16th!";
+session_start();
+require_once "../config/config.php";
+require_once "../src/templates/header.php";
+
+
+
+
+
+
+require_once "../src/templates/footer.html";
