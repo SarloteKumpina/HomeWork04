f5e41774c05a5c48d04bb363ace4a862325a5cb3
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-16 22:27:52 +0300

    added favicon

--- src/templates/header.php
+++ src/templates/header.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="keywords" content="to do list">
+    <title>To Do List</title>
+    <link rel="shortcut icon" href="img/favicon-32x32.png" type="image/x-icon">
     <link rel="stylesheet" href="style/style.css">
     <script src="javascript/javascript.js" defer></script>
 </head>
