22e1699f1aacfcd1cbd24668a267ffee3ae4d354
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-19 21:57:52 +0300

    uninstalled extention

--- src/templates/header.php
+++ src/templates/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="to do list">
     <title>To Do List</title>
-    <link rel="shortcut icon" href="favicon-32x32.png" type="image/x-icon">
+    <link rel="shortcut icon" href="templates/favicon-32x32.png" type="image/x-icon">
     <link rel="stylesheet" href="style/style.css">
     <script src="javascript/javascript.js" defer></script>
 </head>
