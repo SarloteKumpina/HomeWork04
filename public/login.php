b3097317fb3608b57dbef7c8defb877a7a299a06
Šarlote Kumpiņa <sarlote.kumpina@gmail.com>
2020-06-20 00:07:13 +0300

    added sign up button

--- public/login.php
+++ public/login.php
@@ -1,20 +1,23 @@
 <?php
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    require_once "../config/config.php";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $stmt = $conn->prepare("SELECT * FROM users WHERE user_name = (?)");
-    $stmt->bind_param("s", $_POST['userName']);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        // var_dump($row);
-        // die("for now");
-        $_SESSION['user'] = $row['user_name'];
-        $_SESSION['id'] = $row['user_id'];
+    if(isset($_POST['login'])) {
+        require_once "../config/config.php";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $stmt = $conn->prepare("SELECT * FROM users WHERE user_name = (?)");
+        $stmt->bind_param("s", $_POST['userName']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            // var_dump($row);
+            // die("for now");
+            $_SESSION['user'] = $row['user_name'];
+            $_SESSION['id'] = $row['user_id'];
+        }
     }
 }
+
 
 header("Location: /");
